441a844e32e50aaa8f17329f135d9952161c80d9
Jackson Reis <jreis3607@gmail.com>
2025-07-15 08:52:17 -0300

    feat: implementar atualização de produto com validação e mapeamento de DTO
    - Adiciona método updateProduct no ProductService
    - Valida entrada nula e trata ausência do produto com exceção ainda não customizada
    - Atualiza entidade existente com novos dados
    - Retorna DTO de resposta com dados atualizados

--- inventory-service/src/main/java/com/jackson/eventdriven/inventory/service/ProductService.java
+++ inventory-service/src/main/java/com/jackson/eventdriven/inventory/service/ProductService.java
@@ -7,6 +7,7 @@ import com.jackson.eventdriven.inventory.dto.ProductResponseDTO;
 import com.jackson.eventdriven.inventory.entity.ProductEntity;
 import com.jackson.eventdriven.inventory.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -39,5 +40,21 @@ public class ProductService {
         return productRepository.findById(id)
                 .map(ProductMapper :: toDTO)
                 .orElseThrow(() -> new EntityNotFoundException("Produto não encontrado com o id informado"));
+    }
+
+    public ProductResponseDTO updateProduct(UUID id, ProductRequestDTO productRequestDTOUpdated){
+        if (productRequestDTOUpdated == null){
+            throw new IllegalArgumentException("Dados do produto para atualização não podem ser nulos");
+        }
+
+        ProductEntity productExisting = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("produto com o id " + id + " não foi encontrado."));
+
+        productExisting.setName(productRequestDTOUpdated.getName());
+        productExisting.setAmount(productRequestDTOUpdated.getAmount());
+        productExisting.setPrice(productRequestDTOUpdated.getPrice());
+        productRepository.save(productExisting);
+
+        return ProductMapper.toDTO(productExisting);
     }
 }
